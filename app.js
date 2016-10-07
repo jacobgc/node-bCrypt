b10aa08e76330238ed2a96e38106d0c0f6f4bf28
Jacob Gee-Clarke <jacob.clarke2@gmail.com>
2016-10-07 14:45:14 +0100

    Whole bCrypt sample

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var session = require('express-session');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
@@ -20,6 +21,15 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(session({
+  secret: 'IHaveCripplingDepression',
+  resave: true,
+  saveUninitialized: true,
+  cookie: {
+    secure: true,
+    maxAge: 6000000
+  }
+}));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,45 @@
 var express = require('express');
 var router = express.Router();
+var bCrypt = require('bcrypt-nodejs');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'bCrypt -- Select operation' });
+});
+
+/* GET Encrypt page. */
+router.get('/encrypt', function(req, res, next) {
+  res.render('encrypt', { title: 'bCrypt -- Encrypt' });
+});
+
+/* POST Encrypt page */
+router.post('/encrypt', function(req, res, next){
+  var sess = req.session;
+  sess.result = bCrypt.hashSync(req.body.text, bCrypt.genSaltSync(8), null);
+  res.redirect('/result?result=' + sess.result);
+});
+
+/* GET Decrypt page. */
+router.get('/decrypt', function(req, res, next) {
+  res.render('decrypt', { title: 'bCrypt -- Hash Compare' });
+});
+
+/* POST Decrypt page. */
+router.post('/decrypt', function(req, res, next) {
+  var result = bCrypt.compareSync(req.body.text, req.body.hash);
+  if (result === true){
+    res.redirect('/result?result=The%20matches%20the%20hash!');
+  }else{
+    res.redirect('/result?result=The%20does%20not%20match%20the%20hash!');
+  }
+});
+
+
+/* GET result page. */
+router.get('/result', function(req, res, next) {
+  var sess = req.session;
+  res.render('result', { title: 'bCrypt -- Result', req: req, result: req.query.result});
+  console.log(sess.result);
 });
 
 module.exports = router;
