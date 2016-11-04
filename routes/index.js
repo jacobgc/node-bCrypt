9ad47b44b71b3b46eabfd4571f08050588cd7f98
Jacob <jacob.clarke2@gmail.com>
2016-11-04 07:53:07 -0700

    ayy lmao

--- routes/index.js
+++ routes/index.js
@@ -7,39 +7,21 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'bCrypt -- Select operation' });
 });
 
-/* GET Hash page. */
-router.get('/hash', function(req, res, next) {
-  res.render('hash', { title: 'bCrypt -- Hash' });
-});
-
 /* POST Hash page */
 router.post('/hash', function(req, res, next){
   var sess = req.session;
   sess.result = bCrypt.hashSync(req.body.text, bCrypt.genSaltSync(8), null);
-  res.redirect('/result?result=' + sess.result);
-});
-
-/* GET Compare Hash page. */
-router.get('/compare-hash', function(req, res, next) {
-  res.render('compare-hash', { title: 'bCrypt -- Hash Compare' });
+  res.send(sess.result);
 });
 
 /* POST Compare Hash page. */
 router.post('/compare-hash', function(req, res, next) {
   var result = bCrypt.compareSync(req.body.text, req.body.hash);
   if (result === true){
-    res.redirect('/result?result=The%20text%20matches%20the%20hash!');
+    res.send('TRUE');
   }else{
-    res.redirect('/result?result=The%20does%20not%20match%20the%20hash!');
+    res.send('FALSE');
   }
-});
-
-
-/* GET result page. */
-router.get('/result', function(req, res, next) {
-  var sess = req.session;
-  res.render('result', { title: 'bCrypt -- Result', req: req, result: req.query.result});
-  console.log(sess.result);
 });
 
 module.exports = router;
